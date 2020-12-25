37140a4f434a6b1054c480fad3a3be5c634e260a
Vasilev Vasiliy <vvvasiliy1337@gmail.com>
2020-12-25 21:15:46 +0300

    Add AllTasks day1complete day1 tsks

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
+      int count = 0;
 
+      while(count<10) {
+          System.out.println("JAVA");
+          count++;
+      }
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
+       int k = 7;
 
+       for(int count = 1; count<10; count++) {
+           System.out.println(count + " x k = " + k*count);
+       }
     }
 }
