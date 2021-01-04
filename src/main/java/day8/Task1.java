b46f9bc2146a91d40b981343fe74d9cea3ea9b3c
Vasilev Vasiliy <vvvasiliy1337@gmail.com>
2021-01-04 19:52:41 +0300

    AllTasks Day8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,16 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String string = "";
+        for (int a = 0; a <= 20000; a++) {
+            string = string + " " + a + " ";
+        }
+        System.out.println(string);
 
+        StringBuilder s = new StringBuilder();
+        for (int a = 0; a <= 20000; a++) {
+            s.append(a).append(" ");
+        }
+        System.out.println(s.toString());
     }
 }
