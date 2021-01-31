c72144de451459f5d6a79c63cbff914753c6fdca
Vasilev Vasiliy <vvvasiliy1337@gmail.com>
2021-01-31 23:01:34 +0300

    Task Day15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,30 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File fileShoes = new File("D:\\Java\\JavaMarafet2020\\JavaMarathon2020\\src\\main\\resources\\shoes.csv");
+        Scanner scan = new Scanner(fileShoes);
+        List<String> list = new ArrayList<>();
+        while (scan.hasNextLine()) {
+            String string = scan.nextLine();
+            String[] array = string.split(";");
+            if (Integer.parseInt(array[2]) == 0) {
+                list.add(string);
+            }
+        }
+        File ZeroShoes = new File("D:\\Java\\JavaMarafet2020\\JavaMarathon2020\\src\\main\\resources\\missing_shoes");
+        PrintWriter writeShoes = new PrintWriter(ZeroShoes);
+        for (String a : list) {
+            writeShoes.println(a);
+        }
+        writeShoes.close();
     }
 }
+
